0ad6695a09126a2998d92a4839ed37b9c61d0cd1
JuHye Mun <bluemoonjm@gmail.com>
2017-07-19 00:04:45 -0700

    Update canvas.js
    commented out

--- canvas.js
+++ canvas.js
@@ -7,24 +7,24 @@ var c = canvas.getContext('2d');
 
 //Rectangle
 
-c.fillRect(100, 100, 100, 100);
-console.log(canvas);
+//c.fillRect(100, 100, 100, 100);
+//console.log(canvas);
 
 //Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.strokeStyle = "#fa34a3";
-c.stroke();
+//c.beginPath();
+//c.moveTo(50, 300);
+//c.lineTo(300, 100);
+//c.strokeStyle = "#fa34a3";
+//c.stroke();
 
 //arc
-for(var i=0; i<5; i++){
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.stroke();
-}
+//for(var i=0; i<5; i++){
+  //var x = Math.random() * window.innerWidth;
+  //var y = Math.random() * window.innerHeight;
+//  c.beginPath();
+  //c.arc(x, y, 30, 0, Math.PI * 2, false);
+ // c.stroke();
+//}
 
 var x = 200;
 var dx = 4;
