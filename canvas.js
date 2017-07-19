0c7b739a181ea67d580bbb25163c815ae7fc9deb
JuHye Mun <bluemoonjm@gmail.com>
2017-07-18 23:31:13 -0700

    Update canvas.js
    arcs

--- canvas.js
+++ canvas.js
@@ -1,1 +1,14 @@
-console.log('Hi. My name is JuHye')
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+var c = canvas.getContext('2D');
+
+for(var i=0; i<5; i++){
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.arc(x, y, 0, Math.pi * 2, false);
+  
+}
