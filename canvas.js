71cd6ee3ff0b6c59c6e82f5170b8b79fabeea78f
JuHye Mun <bluemoonjm@gmail.com>
2017-07-19 00:55:26 -0700

    Update canvas.js
    animate lots of circles

--- canvas.js
+++ canvas.js
@@ -27,18 +27,54 @@ var c = canvas.getContext('2d');
 //}
 
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
-var radius = 30;
-function animate(){
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+  }
+  this.update = function() {
+  if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+    this.dx = -this.dx;
+  }
+  if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+    this.dy = -this.dy;
+  }
+  this.x += this.dx;
+  this.y += this.dy;
+    
+  this.draw();
+  }
+}
+
+var circleArray = [];
+
+for (var i = 0;  i < 100; i++) {
+  var x = Math.random() * innerWidth;
+  var y = Math.random() * innerHeight;
+  var dx = (Math.random() - 0.5) * 8;
+  var dy = (Math.random() - 0.5) * 8;
+  var radius = 30;
+  circleArray.push(new Circle(x, y, dx, dy, radius)) 
+}
+
+
+function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = 'blue';
-  c.stroke();
+  
+  for (var i = 0;  i < circleArray.length; i++) {
+    circleArray[i].update();
+  
+  circle.update();
   
   if(x + radius > innerWidth || x - radius < 0){
     dx = -dx;
@@ -47,5 +83,5 @@ function animate(){
     dy = -dy;
   }
   x += dx;
-  
+  y += dy;
 }
