a9cc1d634a2aefddbd052cd2b0f02805aefbaacc
JuHye Mun <bluemoonjm@gmail.com>
2017-07-19 00:16:55 -0700

    Update canvas.js
    animate y too

--- canvas.js
+++ canvas.js
@@ -8,7 +8,7 @@ var c = canvas.getContext('2d');
 //Rectangle
 
 //c.fillRect(100, 100, 100, 100);
-//console.log(canvas);
+console.log(canvas);
 
 //Line
 //c.beginPath();
@@ -26,19 +26,26 @@ var c = canvas.getContext('2d');
  // c.stroke();
 //}
 
-var x = 200;
-var dx = 4;
+
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 8;
+var dy = (Math.random() - 0.5) * 8;
 var radius = 30;
 function animate(){
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   c.beginPath();
-  c.arc(x, 200, radius, 0, Math.PI * 2, false);
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
   c.strokeStyle = 'blue';
   c.stroke();
   
-  if (x + radius > innerWidth || x - radius < 0){
+  if(x + radius > innerWidth || x - radius < 0){
     dx = -dx;
   }
+  if(y + radius > innerHeight || y - radius < 0){
+    dy = -dy;
+  }
   x += dx;
+  
 }
