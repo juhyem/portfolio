8d810d09b9897ee5e7cd9f8af4551f7bb2f1e561
JuHye Mun <bluemoonjm@gmail.com>
2017-07-18 23:44:22 -0700

    Update canvas.js
    rect and line

--- canvas.js
+++ canvas.js
@@ -5,6 +5,18 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2D');
 
+//Rectangle
+c.fillStyle = "#fa34a3";
+c.fillRect(100, 100, 100, 100);
+
+//Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.strokeStyle = "#fa34a3";
+c.stroke();
+
+//arc
 for(var i=0; i<5; i++){
   var x = Math.random() * window.innerWidth;
   var y = Math.random() * window.innerHeight;
