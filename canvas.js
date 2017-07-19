f95b5377465ec86a18affb471b4b040aded937ab
JuHye Mun <bluemoonjm@gmail.com>
2017-07-18 23:36:27 -0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2D');
 
